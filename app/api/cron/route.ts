a416f780f6ce5e10e10a04a4fddb583b61983f65
Veeresh R G <veeresh.ravi@gmail.com>
2024-06-04 19:36:11 +0530

    We are close !!!

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,20 +1,27 @@
 import axios, {AxiosResponse} from 'axios';
 import getAccessToken from "@/util/access_token"
 import prisma from '@/db';
-import { Request, Response } from 'express';
+import { NextRequest, NextResponse } from 'next/server';
 
 
-export async function PUT(req : Request , resp : Response) { // Add the 'Request' type as a parameter
+export async function PUT(req : NextRequest) { // Add the 'Request' type as a parameter
   try {
     
     // console.log(accessToken);
-    const data :any = req.body;
-    console.log(data);
+    const d = await req.json();
+    console.log(d);
+    
+    
+    // const data :any = await req.json();
+    // console.log(data);
     const updatedRunner = await prisma.runner.update({
-      where: { athelete_name: data.name },
-      data: { total_kilometers : data.distance },
+      where: { athelete_name: d?.name },
+      data: { total_kilometers : d?.distance },
     });
-    return resp.json("hello");
+  
+    return NextResponse.json({"Hello" : "World"})
+
+
   } catch (err : any) {
     console.error('Error updating runners:', err.response.data);
 
--- server/server.js
+++ server/server.js
@@ -3,31 +3,30 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //route that retrieves access token in exchange of auth code on login
 app.post("/login", (req, res) => {
-    const code = req.body.code
-    let headersList = {
-        Accept: "*/*",
-        "Content-Type": "application/x-www-form-urlencoded",
-      };
-    let bodyContent = `client_id=125102&client_secret=baf7f62b9ccc8636309d04404f9673f3c8bc9f95&code=${code}&grant_type=authorization_code`;
+  const code = req.body.code;
+  let headersList = {
+    Accept: "*/*",
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+  let bodyContent = `client_id=125102&client_secret=baf7f62b9ccc8636309d04404f9673f3c8bc9f95&code=${code}&grant_type=authorization_code`;
 
-    fetch("https://www.strava.com/oauth/token", {
-        method: "POST",
-        body: bodyContent,
-        headers: headersList,
+  fetch("https://www.strava.com/oauth/token", {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      res.json(data);
     })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        res.json(data)
-    })
-    .catch(error => console.error(error))
-})
+    .catch((error) => console.error(error));
+});
 
-
-app.listen(3001)
+app.listen(3001);
