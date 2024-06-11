e1b78ab6be4852f9a076a0c0f769487b1b371f83
Veeresh R G <veeresh.ravi@gmail.com>
2024-06-11 19:46:58 +0530

    [ERROR RESOLVE] Window Object

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,50 @@
 'use client'
 import React from 'react';
-import { ReactDOM } from 'react';
 import Login from '@/components/Login';
 import Home from '@/components/Home';
+// import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 //On auth user redirected back to index url with code as query paramater in URL
-const code = new URLSearchParams(window.location.search).get("code")
 
-const App: React.FC = () => {
+
+
+
+const App = () => {
+  // const code = new URLSearchParams(window.location.search).get("code")
+  // const router = useRouter();
+  // const [query, setQuery] = useState<any | null>(null);
+
+  // useEffect(() => {
+  //   if (router.isReady) {
+  //     setQuery(router.query);
+  //   }
+  // }, [router.isReady, router.query]);
+
+  const [code, setCode] = useState<string | null>("")
+
+  useEffect(() => {
+    if (typeof window === "object") {
+
+      // console.log(new URLSearchParams(window.location.search).get("code"))
+
+      setCode(new URLSearchParams(window?.location?.search).get("code"))
+    }
+  }, [code])
+
+  console.log(code);
+
+
   return (
-    code ? <Home authCode={code}/> : <Login/>
+    <div>
+      {
+        code ? <Home authCode={code} /> : <Login />
+
+      }
+    </div>
+    // <div>
+    //   hola {""}
+    // </div>
   );
 }
 
