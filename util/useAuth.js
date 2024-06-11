c839241ab5ddc4c093b24b6b1f8a876884da7cf7
Veeresh R G <veeresh.ravi@gmail.com>
2024-06-11 21:54:21 +0530

    [EDIT] Change useAuth Endpoint

--- util/useAuth.js
+++ util/useAuth.js
@@ -1,22 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function useAuth(code){
-    const [authInfo, setAuthInfo] = useState()
+export default function useAuth(code) {
+  const [authInfo, setAuthInfo] = useState();
 
-    //Send post request to server endpoint with auth code to get short-lived access token
-    useEffect(() =>{
-      axios.post("http://localhost:3001/login", {
-        code
-      }).then(res => {
-        setAuthInfo(res.data)
-        window.history.pushState({}, null, "/")
-      }).catch(error => {
-        console.error(error)
-        //On error redirect back to Login
-        window.location = "/"
+  //Send post request to server endpoint with auth code to get short-lived access token
+  useEffect(() => {
+    axios
+      .post("https://microservice-strava-dashboard.vercel.app/login", {
+        code,
       })
-    }, [code])
+      .then((res) => {
+        setAuthInfo(res.data);
+        window.history.pushState({}, null, "/");
+      })
+      .catch((error) => {
+        console.error(error);
+        //On error redirect back to Login
+        window.location = "/";
+      });
+  }, [code]);
 
-    return authInfo
+  return authInfo;
 }
