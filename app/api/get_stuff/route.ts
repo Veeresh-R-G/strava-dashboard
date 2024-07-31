59f68fd2af561ad054c68496c13d8d19402b85e4
Veeresh R G <veeresh.ravi@gmail.com>
2024-07-31 20:48:29 +0530

    check caching - 3

--- app/api/get_stuff/route.ts
+++ app/api/get_stuff/route.ts
@@ -4,11 +4,14 @@ import prisma from '@/db';
 
 export async function GET(){
     try{
+
+      console.log("/get_stuff");
+      
       const resp = await prisma.runner.findMany()
       
       console.log(resp);
       
-      return Response.json({"data" : resp})
+      return Response.json({"data" : resp, "message":"All Runners data fetched ✅"})
   }
   catch (error: any) {
       console.log('Error fetching data:', error);
