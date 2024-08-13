ea632737bcca7936d032d14706c68217cc1c2bea
Veeresh R G <veeresh.ravi@gmail.com>
2024-08-13 12:54:20 +0530

    [Rules] Contribution

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,8 @@
-'use client'
-
-import NavbarComponent from "@/components/Navbar";
 import React from "react";
 
 export default function App() {
     return (
-        <div className="w-full">
-            <NavbarComponent />
+        <div className="">
             This is Test Component
         </div>
     );
