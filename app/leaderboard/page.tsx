2e944c2cdbf1af5e69862c87ec334a26d51e0e54
Veeresh R G <veeresh.ravi@gmail.com>
2024-08-14 19:29:22 +0530

    [FIX] Leaderboard Loader

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -62,80 +62,81 @@ const Leaderboard = () => {
         }} />
 
       </div>
+      <div className='flex items-center justify-center'>
 
-      {data.length !== 0 ?
+        {data.length !== 0 ?
 
-        <div className='mt-4 flex items-center justify-center'>
+          // Leaderboard Table
+          <div className='mt-4'>
 
-          {
-            new Date().getDate() === new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate() && <Fireworks autorun={{ speed: 3 }} />
-          }
+            {
+              new Date().getDate() === new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate() && <Fireworks autorun={{ speed: 3 }} />
+            }
 
-          <div className='leaderboard-table'>
+            <div className='leaderboard-table'>
+              <Table aria-label="" className=''>
+                <TableHeader>
+                  <TableColumn>#</TableColumn>
+                  <TableColumn>Name</TableColumn>
+                  <TableColumn>{""}</TableColumn>
+                  <TableColumn>Distance</TableColumn>
+                </TableHeader>
+                <TableBody>
+                  {data?.slice((currentPage - 1) * (per_page), ((currentPage - 1) * (per_page)) + per_page).map((item: any, index: number) => {
+                    return (
+                      <TableRow key={index}>
+                        <TableCell className=''>{((currentPage - 1) * (per_page)) + index + 1}.</TableCell>
+                        <TableCell className='text-wrap'> {currentPage === 1 ? index == 0 ? <FaTrophy className='text-amber-400 inline mr-2' /> : index == 1 ?
+                          <FaTrophy className='text-neutral-700 inline mr-2' /> : index === 2 ?
+                            <FaTrophy className='text-yellow-900 inline mr-2' /> : "" : ""}
+                          {item.athelete_name}
+                        </TableCell>
+                        <TableCell className=''>
+                          <Image src={item.photoUrl} alt="photo" width={40} height={40} className='rounded-full' />
+                        </TableCell>
+                        <TableCell className=''>
+                          {(String(Number(item.total_kilometers) / 1000)).slice(0, 5)}
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })}
 
+                </TableBody>
+              </Table>
 
-
-            <Table aria-label="" className=''>
-              <TableHeader>
-                <TableColumn>#</TableColumn>
-                <TableColumn>Name</TableColumn>
-                <TableColumn>{""}</TableColumn>
-                <TableColumn>Distance</TableColumn>
-              </TableHeader>
-              <TableBody>
-                {data?.slice((currentPage - 1) * (per_page), ((currentPage - 1) * (per_page)) + per_page).map((item: any, index: number) => {
+              {/* Table content */}
+              <div className='hidden'>
+                {data?.slice((currentPage - 1) * (per_page), ((currentPage - 1) * (per_page)) + per_page).map((item: MyObject, index: number) => {
                   return (
-                    <TableRow key={index}>
-                      <TableCell className=''>{((currentPage - 1) * (per_page)) + index + 1}.</TableCell>
-                      <TableCell className='text-wrap'> {currentPage === 1 ? index == 0 ? <FaTrophy className='text-amber-400 inline mr-2' /> : index == 1 ?
-                        <FaTrophy className='text-neutral-700 inline mr-2' /> : index === 2 ?
-                          <FaTrophy className='text-yellow-900 inline mr-2' /> : "" : ""}
+
+                    <div key={index} className='flex'>
+                      <div className='bg-gray-100/60 p-2 pl-10 border text-xs md:text-base border-t-0 border-r-0 border-l-0 border-gray-300'>{((currentPage - 1) * (per_page)) + index + 1}.</div>
+                      <div className='bg-gray-100/60 w-20 text-xs md:text-base md:pl-10 md:w-48 border border-t-0 border-r-0 border-l-0 border-gray-300 py-2'>
+                        {currentPage === 1 ? index == 0 ? <FaTrophy className='text-amber-400 inline mr-2' /> : index == 1 ?
+                          <FaTrophy className='text-neutral-700 inline mr-2' /> : index === 2 ?
+                            <FaTrophy className='text-yellow-900 inline mr-2' /> : "" : ""}
                         {item.athelete_name}
-                      </TableCell>
-                      <TableCell className=''>
+                      </div>
+                      <div className='bg-gray-100/60 w-20 text-xs md:text-base md:pl-10 md:w-48 border border-t-0 border-r-0 border-l-0 border-gray-300 py-2 flex justify-center'>
                         <Image src={item.photoUrl} alt="photo" width={40} height={40} className='rounded-full' />
-                      </TableCell>
-                      <TableCell className=''>
-                        {(String(Number(item.total_kilometers) / 1000)).slice(0, 5)}
-                      </TableCell>
-                    </TableRow>
+                      </div>
+                      <div className='bg-gray-100/60 w-20 text-xs text-center md:text-base md:pl-10 md:w-48 border border-t-0 border-r-0 border-l-0 border-gray-300 py-2'>{(String(Number(item.total_kilometers) / 1000)).slice(0, 5)}</div>
+                    </div>
                   )
                 })}
+              </div>
 
-              </TableBody>
-            </Table>
-
-            {/* Table content */}
-            <div className='hidden'>
-              {data?.slice((currentPage - 1) * (per_page), ((currentPage - 1) * (per_page)) + per_page).map((item: MyObject, index: number) => {
-                return (
-
-                  <div key={index} className='flex'>
-                    <div className='bg-gray-100/60 p-2 pl-10 border text-xs md:text-base border-t-0 border-r-0 border-l-0 border-gray-300'>{((currentPage - 1) * (per_page)) + index + 1}.</div>
-                    <div className='bg-gray-100/60 w-20 text-xs md:text-base md:pl-10 md:w-48 border border-t-0 border-r-0 border-l-0 border-gray-300 py-2'>
-                      {currentPage === 1 ? index == 0 ? <FaTrophy className='text-amber-400 inline mr-2' /> : index == 1 ?
-                        <FaTrophy className='text-neutral-700 inline mr-2' /> : index === 2 ?
-                          <FaTrophy className='text-yellow-900 inline mr-2' /> : "" : ""}
-                      {item.athelete_name}
-                    </div>
-                    <div className='bg-gray-100/60 w-20 text-xs md:text-base md:pl-10 md:w-48 border border-t-0 border-r-0 border-l-0 border-gray-300 py-2 flex justify-center'>
-                      <Image src={item.photoUrl} alt="photo" width={40} height={40} className='rounded-full' />
-                    </div>
-                    <div className='bg-gray-100/60 w-20 text-xs text-center md:text-base md:pl-10 md:w-48 border border-t-0 border-r-0 border-l-0 border-gray-300 py-2'>{(String(Number(item.total_kilometers) / 1000)).slice(0, 5)}</div>
-                  </div>
-                )
-              })}
             </div>
 
+          </div> :
+
+          // Loader component
+          <div>
+            <Spinner size='lg' label='Loading...' color={spinnerColors[Math.floor(Math.random() * spinnerColors.length)] as "primary" | "secondary" | "success" | "warning" | "current" | "white" | "default" | "danger" | undefined} />
           </div>
 
-
-        </div>
-        :
-        <div>
-          <Spinner size='lg' label='Loading...' color={spinnerColors[Math.floor(Math.random() * spinnerColors.length)] as "primary" | "secondary" | "success" | "warning" | "current" | "white" | "default" | "danger" | undefined} />
-        </div>
-      }
+        }
+      </div>
     </div>
   );
 };
